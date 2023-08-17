c291cbc4dde9d0fca7f167dfd6831a9ca59dceb4
Satyaprakash7 <satyaprakash.sahoo840@gmail.com>
2023-08-17 15:48:03 +0530

    edit

--- server.js
+++ server.js
@@ -9,23 +9,12 @@ const billsRoute = require("./routes/billsRoute");
 app.use("/api/items/", itemsRoute);
 app.use("/api/users/", usersRoute);
 app.use("/api/bills/", billsRoute);
-// const path = require('path')
-
-// if(process.env.NODE_ENV==='production')
-// {
-//     app.use('/' , express.static('client/build'))
-//     app.get('*' , (req,res)=>{
-//          res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
-//     })
-// }
-// deployment config
 const path = require("path");
-__dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use("/", express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
 }
 
